c99eb322d306e2969619c53dec439c61b2979626
mlo77 <lyy.chu@gmail.com>
2013-07-16 00:54:06 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	var c0 = document.getElementById('c0');
+	c0.className = 'imgroll_elmt';
+	var im=document.createElement('img');
+	im.src='img/test/IMG_5823.JPG';
+	c0.appendChild(im);
+	//alert("aaaaa");
+};
+
